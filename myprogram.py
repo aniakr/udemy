1a1b8fe1bcdd2cfbc2cf3e6f5922b0a670cab46f
aniakr <anna.krach@gmail.com>
2018-07-21 14:52:43 +0200

    exercises clean up 1

--- myprogram.py
+++
@@ -1,9 +0,0 @@
-# dict={"Name":"Ania","Surname":"Krach","cities":("Porto","Chicago","Kraków")}
-# print(dict["cities"][2])
-a=1
-b="2"
-print(int(2.5)
-print(a+b)
-
-# blad pokaze na koncu drugiego print, bo pokazuje ze drugi print jest w zlym miejscu,
-# a tam powinien byc nawias zamykajacy
